07d551dd9e4cf6d27ab711b224d8b5b2c9049406
Mickaël Canévet <mickael.canevet@camptocamp.com>
2013-10-04 09:34:25 +0200

    Fix: Fail catalogue if postgresql::server::manage_pga_conf is disabled

--- manifests/server/pg_hba_rule.pp
+++ manifests/server/pg_hba_rule.pp
@@ -15,7 +15,7 @@ define postgresql::server::pg_hba_rule(
   $target      = $postgresql::server::pg_hba_conf_path
 ) {
 
-  if $postgresql::server::manage_pga_conf != true {
+  if $postgresql::server::manage_pga_conf == false {
       fail('postgresql::server::manage_pga_conf has been disabled, so this resource is now unused and redundant, either enable that option or remove this resource from your manifests')
   }
 
